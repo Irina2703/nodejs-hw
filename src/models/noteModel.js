f699f301006ea1fd735b95bcdbc63fa2569809a6
Kliucherova Irina <klucerovaira@gmail.com>
2025-10-14 15:39:04 +0200

    commit

--- src/models/noteModel.js
+++ src/models/noteModel.js
@@ -1,13 +1,14 @@
-// У файлі models/noteModel.js
 import mongoose from 'mongoose';
 
 const noteSchema = new mongoose.Schema({
-    title: String,
-    content: String,
-    tag: String,
+    title: { type: String, required: true },
+    content: { type: String, default: '' },
+    tag: { type: String, required: true },
 }, { timestamps: true });
 
-// Створення текстового індексу для пошуку по title та content
+
 noteSchema.index({ title: 'text', content: 'text' });
 
-export default mongoose.model('Note', noteSchema);
+const Note = mongoose.model('Note', noteSchema);
+
+export default Note;
